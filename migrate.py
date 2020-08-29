64ea105c11cc54f7993662d350b39f1eb69651cc
Tatiana Malinovskaya <Tatyana Malinovskaya>
2020-08-29 20:58:48 +0200

    Created sample user and review

--- migrate.py
+++ migrate.py
@@ -1,5 +1,6 @@
 import csv
 import os
+import random
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
@@ -68,13 +69,45 @@ def main():
 
     if books_filled():
         return
+
+    print("Inserting books...")
     with open("books.csv") as opned_file:
         reader = csv.reader(opned_file)
         next(reader, None)
         for isbn, title, author, year in reader:
-            db.execute("INSERT INTO books (isbn, title, author, year) VALUES (:isbn, :title, :author, :year)",
-                {"isbn": isbn, "title": title, "author": author, "year": int(year)})
-        db.commit()
+            db.execute(
+            """
+            INSERT INTO books (isbn, title, author, year)
+            VALUES (:isbn, :title, :author, :year)
+            """,
+            {"isbn": isbn, "title": title, "author": author, "year": int(year)}
+            )
+            db.commit()
+
+    print("Creating sample user...")
+    test_user = db.execute(
+    """
+    INSERT INTO users (name, email, password)
+    VALUES (:name, :email, :password)
+    RETURNING user_id
+    """,
+    {"name": "Bob", "email": "test@test.com", "password": "password"}
+    ).fetchone()
+    db.commit()
+
+    print("Inserting review...")
+    db.execute(
+    """
+    INSERT INTO reviews (isbn, user_id, rate, revision_text)
+    VALUES
+    (:isbn, :user_id, :rate, :revision_text)
+    """,
+    {"isbn": 0380795272,
+    "user_id": test_user.user_id,
+    "rate": 4,
+    "revision_text": "Some text to demonstrate how a book page with a review looks"}
+    )
+    db.commit()
 
 
 if __name__ == "__main__":
